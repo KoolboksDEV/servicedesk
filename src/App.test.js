a48c2f7b52799829968a57f8cdf3ecaae7a1c220
KoolboksDEV <162709521+KoolboksDEV@users.noreply.github.com>
2025-07-08 11:41:19 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders Unified Service Management heading on landing page', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  );
-  const heading = screen.getByText(/Unified Service Management/i);
-  expect(heading).toBeInTheDocument();
-});
-
 test('renders Get started button on landing page', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  );
-  const button = screen.getByRole('button', { name: /Get started/i });
+  render(<App />);
+  const button = screen.getByRole('button', { name: /get started/i });
   expect(button).toBeInTheDocument();
 });
